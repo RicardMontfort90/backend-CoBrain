abec98c8743c06d70db3273d146155a278e12d54
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-28 17:15:42 +0200

    WORK IN PROGRESS

--- routes/knowledges.js
+++ routes/knowledges.js
@@ -18,3 +18,23 @@ router.get('/', isAuthenticated, async (req, res, next) => {
             next(error);
         }
 });
+
+// @desc    GET single bike
+// @route   GET /api/v1/
+// @access  Public
+router.get('/:id', isAuthenticated, async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const knowledge = await Knowledge.findById(id);
+        if(!knowledge) {
+            return next(new ErrorResponse('knowledge not found', 404));
+        } 
+        res.status(200).json({ data: knowledge })
+    } 
+    catch (error) {
+        next(error);
+    }
+});
+
+
+module.exports = router;
