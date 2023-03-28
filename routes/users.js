bc81f4bf9ddbd038ff1fd58af54a05bc16f5bf5b
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-03-28 17:33:01 +0200

    WORK IN PROGRESS

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,47 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticated, isAdmin } = require('../middlewares/jwt');
 const User = require("../models/User");
+const Review = require('../models/Review');
+const { isAuthenticated, isAdmin } = require('../middlewares/jwt');
+
+
+// @desc    Edit User
+// @route   PUT /api/v1/
+// @access  Public
+router.put('/', isAuthenticated, async (req, res, next) => {
+    const userId = req.payload._id;
+    const { email, hashedPassword, username } = req.body;
+    try {
+        const user = await User.findById(userId);
+        if(!user) {
+            return next(new ErrorResponse(`User not found by ${userId}`, 404));
+        } 
+        else {
+            const updatedUser = await User.findByIdAndUpdate(userId, { email, hashedPassword, username }, {new: true});
+            res.status(202).json({ data: updatedUser })
+        }
+    } 
+    catch (error) {
+        next(error);
+    }
+});
+
+// @desc    Delete User
+// @route   DELETE /api/v1/
+// @access  Public
+router.delete('/', isAuthenticated, async (req, res, next) => {
+    const userId = req.payload._id;
+    try {
+        const user = await User.findById(userId);
+        if(!user) {
+            return next(new ErrorResponse(`User not found by ${userId}`, 404));
+        } else {
+            const deletedReview = await Review.deleteMany({userId});
+            const deletedUser = await User.findByIdAndDelete(userId);
+            res.status(202).json({ data: deletedReview, deletedUser })
+        }
+    } catch (error) {
+        next(error);
+    }
+});
 
